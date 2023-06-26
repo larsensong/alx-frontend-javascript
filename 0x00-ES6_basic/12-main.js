b18cadd3a6e6c8fa0a4111606f8c2602eda1a484
root <larsenmulamba@gmail.com>
2023-06-26 21:59:56 +0000

    update

--- 0x00-ES6_basic/12-main.js
+++ 0x00-ES6_basic/12-main.js
@@ -1,4 +1,4 @@
-iimport createEmployeesObject from './11-createEmployeesObject.js';
+import createEmployeesObject from './11-createEmployeesObject.js';
 import createReportObject from './12-createReportObject.js';
 
 const employees = {
